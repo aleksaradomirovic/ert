c90a97ba5b91246f7c420bbcfe2e2d3d6a2e2401
Aleksa Radomirovic <braleksa2@gmail.com>
2025-08-08 19:26:44 -0700

    Added tests

---
+++ tests/allocator/standard_allocator_test.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2025 Aleksa Radomirovic
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "allocator_test.hpp"
+
+TEST_F(allocator_test, alloc_and_free) {
+    volatile void *mem;
+    ASSERT_EQ(ecr_allocate(&allocator, (void **)(&mem), 16), ECR_SUCCESS);
+    ASSERT_EQ(ecr_free(&allocator, (void *) mem), ECR_SUCCESS);
+}
